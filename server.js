69d309ed9f90762eec1fdeac2375022159cc707f
Humberto <humberto.galdino@live.com>
2021-11-20 23:03:03 -0300

    Configura Servidor

---
+++ server.js
@@ -0,0 +1,23 @@
+//Insere o módulo de express
+const express = require('express');
+
+//Coleta os métodos do express
+const app = express();
+
+//Insere estrutura rotas
+app.use('/', require('./src/routes/routes'));
+
+//Insere estrutura EJS
+app.set('view engine', 'ejs');
+
+//Insere estrutura pasta views
+app.set('views', './src/views');
+
+//Insere estrutura pasta public
+app.use(express.static('public'));
+
+//Permite o express entender as requisições JSON da API
+app.use(express.json());
+
+//Define a porta que servidor irá funcionar
+app.listen(3000, ()=> console.log('Servidor Funcionando!'));
