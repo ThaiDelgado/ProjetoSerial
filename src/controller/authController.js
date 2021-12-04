a8bb98e8301c9bd98ad8be1ce12f13b19a6a1909
Humberto <humberto.galdino@live.com>
2021-12-03 22:00:25 -0300

    Configurando Rotas e Controllers

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,6 +1,9 @@
+//login, logout, recuperação de senha e validação de e-mail
 
 module.exports = {
+    
     login(req,res){
         res.render('login');
     },
+
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,23 +1,21 @@
 //Rota geral
 const express = require("express");
+
+//Responsável por escutar a rota e redirecionar para o método do controller
 const routes = express.Router();
 
-const serialController = require('../controller/serialController')
-const userControler = require('../controller/userController')
-const pgSerieController = require('../controller/pgSerieController')
-const pgEpisodioController = require('../controller/pgEpisodioController')
-
-//Rota inicial
-routes.get('/login', serialController.login);
+const userControler = require('../controller/userController');
+const episodeController = require('../controller/episodeController');
+const serieController = require('../controller/serieController');
 
 //Rota usuário
-routes.get('/usuario', userControler.usuario);
+routes.get('/usuario', userControler.index);
 
 //Rota Episodio
-routes.get('/episodio', pgEpisodioController.episodio);
+routes.get('/episodio', episodeController.index);
 
 //Rota Serie
-routes.get('/serie', pgSerieController.serie)
+routes.get('/serie', serieController.index);
 
 // // rota da página de episodios
 // app.get('/series/episodios/id:', (req, res) => {
