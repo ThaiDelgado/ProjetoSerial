4e6ff02def80181a1f13a9a6a25338b7dbfb7ad9
ThaiDelgado <thaissadelgado@gmail.com>
2022-02-19 12:10:10 -0300

    Exclui confirmação de email

--- src/controller/emailConfirmacao.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-
-    index(req, res){
-        res.send('confirmaEmail')
-    }
-};
--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -6,13 +6,13 @@ const homeController = require('../controller/homeController')
 const loginController = require('../controller/loginController')
 const recuperaSenhaController = require('../controller/recuperaSenhaController')
 const cadastroController = require("../controller/cadastroController");
-const emailConfirmacao = require('../controller/emailConfirmacao');
+
 
 
 routes.get('/', homeController.index);
 routes.get('/login', loginController.index);
 routes.get('/recuperaSenha', recuperaSenhaController.index);
 routes.get('/cadastro', cadastroController.index);
-routes.post('/emailConfirmacao', emailConfirmacao.index);
+
 
 module.exports = routes;
