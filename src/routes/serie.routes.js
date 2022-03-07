cdfcf7b77f15e63c604b6426c8840a09b5d9dd42
Humberto Galdino <humberto.galdino@live.com>
2022-03-07 11:55:33 -0300

    Implementa Lógia Remover Favorito

--- src/routes/serie.routes.js
+++ src/routes/serie.routes.js
@@ -18,11 +18,17 @@ routes.post('/:id/:season', auth, serieController.postComment);
 //Roda Delete Comentário
 routes.delete('/:id/:season/deleteComment/:idComment', auth, serieController.deleteComment);
 
-//Rota Insere Série Favorita
+//Rota Insere Série como favorita
 routes.put('/:id/:season/favorite', auth, serieController.addFavoriteTvShow);
+
+//Rota Remove Série dos favoritos
+routes.delete('/:id/:season/deletefavorite', auth, serieController.removeFavoriteTvShow);
 
 //Rota Insere Série no cast do usuário
 routes.put('/:id/:season/adicionar', auth, serieController.addTvShowToCast);
+
+// //Rota Remove Série do cast do usuário
+// routes.delete('/:id/:season/remover', auth, serieController.removeTvShowToCast);
 
 //Rota Insere Episódio
 routes.put('/:id/:season/:episode_number/:episode_id', auth, serieController.addEpisode);
